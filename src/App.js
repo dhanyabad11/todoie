2a7cdb276a89927d1271fc4373dc51093dad97e8
dhanyabad11 <dhanyabadbehera@gmail.com>
2024-02-27 19:56:24 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from './components/Header';
+import Todoitem from './components/Todoitem';
+import Button from './components/Button';
+import "./style.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const App = () => {
+  return ( <div className="todo-container">
+    <Header />
+  <Todoitem />
+  <Todoitem />
+  <Todoitem />
+  <Todoitem />
+  <Todoitem />
+   <Button />
+   </div>
   );
-}
 
+};
 export default App;
---
+++ src/components/Todoitem.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Todoitem = () => {
+  return (
+    <li className="todo-item"> 
+      <div>
+      <input type="checkbox" />
+      <span>Eat</span>
+      </div>
+      <p>...</p>
+    </li>
+  );
+};
+export default Todoitem;
